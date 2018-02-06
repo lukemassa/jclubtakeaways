7147423cc7166171241604506643531e0099f607
root <root@lmassa-dev.dhcp.tripadvisor.com>
2018-02-06 15:00:08 -0500

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.ElpZBaEXbVjSFZVzsc-eo6fRp--rhomliZxhv9Ke3aYgMxkRtZN5gMTkud1ACSlHqDQQitdOA8iRk8eiOq33pvpabRjzYW5n6NjWA7JDQsaEpfaI6baTbi5wo7U'
+var oauthtoken = 'ya29.c.ElpZBZkNTeo20eJoPFczqwOCXCC8c7DFOB_jFgMguD66lHPt-LJwNVevta3OCZCWQ9L6ILuZOany4m1acUQomFyAGU0t_O4jFM5yBUcPSuM3CY2Rb4ORWSWdEpo'
