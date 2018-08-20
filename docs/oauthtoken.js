a3a2b725026b881a576cb14e7d8d671ed8d5e8c6
root <root@lmassa-dev.dhcp.tripadvisor.com>
2018-08-19 21:00:09 -0400

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.Elr_Bf9MnpXYH0NdUXTQi5mr7S0YcZUGfh0MYhfs6nhCZy_GP7NUHfQIES_rT_VVI64S7VzDgcnZtvz12VKO9FVmBzBuDtwFQmA3a1a-JPkdQ03mqDYM4EOU3HU'
+var oauthtoken = 'ya29.c.Elr_BQ_5E-iI6t88lyOmHLmkcUSOjbvZIldhW4_g9ehRpE-cNWbITTyxsBND2c2Hh_gm3ILJRYXZ7JPyHifckJE6cblgFiEE5gwZ_8_lGksEDTDJCZowmNOqb-4'
