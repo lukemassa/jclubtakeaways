cdb410085e8b40b9f521c66937237116cc5e26d0
root <root@lmassa-dev.dhcp.tripadvisor.com>
2018-09-14 05:00:10 -0400

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.EloYBjJAiuzkEkjz6IUuuQn0G8mD6_mlF_WIOQ0GuaBbUHnHuKIm-FrfjMXeiFTP0kMc_lFo8vh-JbxqnrBvx5JnIndOxiMtgAhA8IdwUjxMdFpqFiz3-d9_7ec'
+var oauthtoken = 'ya29.c.EloYBnPUK90ljpwNjBTSFNqOCNXHFB1dgudmDg1XWNcEcNjFsigDvOCBsh-5tNp0cFwym_axx1_KrpASNPSNCdcVKlbtBadDje1rdTHepMX9nZI9qSxUdjYlcJk'
