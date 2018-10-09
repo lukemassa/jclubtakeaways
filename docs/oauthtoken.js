eddaba5349150b8017c6b42ea78fff74b3d79d25
root <root@lmassa-dev.dhcp.tripadvisor.com>
2018-10-09 16:20:08 -0400

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.EloxBocYstTYOjW3is3B_djiQLRXdzOHo15HtwoJhJ0HomgslDQF6HwOsUhEKkkOVTstiqqPbbgP8ugEZyRNB4uRhRyLxWUf6zASVe8B7XyGCx9BmfckY0YDCno'
+var oauthtoken = 'ya29.c.EloxBm76b18qw9sRhmB3_jo_R3NXjjtrhNrRydTwhjNK_4AeNidCUDG6cMbByWGDsF8fpgl77xxxDQtlxQLR8_KOi1ekVySJ72A-0ZQxyhxz9kRadda5u8VjZaI'
