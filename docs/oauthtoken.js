f313715db3f38ee815d21398f50eac923cf11bad
root <root@lmassa-dev.dhcp.tripadvisor.com>
2018-10-11 11:40:08 -0400

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.ElozBt3gcmaxl_Q2c57QEbzCj5wYtfi3mDESj1AXbcL6veD7hi1S0NmIfoGPSmAGE5G_V1zRxN4HIWPsnOvm1GTOQp3wtUF7_t4C4TQJaXZM5Wh28UtO33A-NEk'
+var oauthtoken = 'ya29.c.ElozBuwcXG9ikRFoaVOUpS7OnKnHmcr34-IoghabQs-dhz_kjysWRmK7cnuH9Tu3anoFSOpFrW2TIh7Xii1pYbYhfLVrqnFE0EUgaTJMy2VK6bHvatysT51_i4w'
