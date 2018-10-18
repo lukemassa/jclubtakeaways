204078e0737d7a91506a3c127ab1a1a3c56eb2a2
root <root@lmassa-dev.dhcp.tripadvisor.com>
2018-10-18 00:20:07 -0400

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.Elo6Bmw8GAx-L069ckcrMk5odYr7VwtbEItZNC8WL9oxxQ-j9jRXgU-R5wdjilHY5P_Oi8A-cjPpuWrt2hrroN1oh2iwWOeCS-9NlNm3rdRDjNXm9IIc9Ailn70'
+var oauthtoken = 'ya29.c.Elo6Bm7b3M51jsCwkoSKJXvaqwm0VTVu7ud64MMn5y_anDWciYFwY5mroxkpJAWVW0mF7kY278jDlAgdkyoEr4YYZX2uG9Pa3I5GllnqiSVylsxq2C2rd-fByKg'
