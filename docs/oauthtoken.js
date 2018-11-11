bacc279676935f0e035ca86e733ffd605c0122aa
root <root@lmassa-dev.dhcp.tripadvisor.com>
2018-11-11 17:40:07 -0500

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.ElpRBg8l9TJke80SHATnw7l1pknChnseDESK37Io9xaua05mvimCrEbhiLRbXMstZla0yKkalVK46Yn6eIA-JEmjZVLel4g666xuP2xI-YRfKWD7jIWF3zPWE_M'
+var oauthtoken = 'ya29.c.ElpRBoZdXRqEkJpUlVXvri7fPwErPTv0ryXkUTP06sZ1pktuY426ihOsweikqESbzciVgAsWUBeeE_DgGYfSLEEYzd8-vOsYs0qbKYE78OdNRoQp7-iZwPSfaUA'
