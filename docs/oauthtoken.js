5a952d7f873b47ec31834bf0a326868694189086
root <root@lmassa-dev.dhcp.tripadvisor.com>
2018-11-25 13:40:08 -0500

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.ElpfBiYFa2GDSL5tpa_huLFSfw10_mQrVx6WjB4nuzWVVy1civFMd2Db3lZmLUxsebrZWfRocMA5Frod9-F8N3YGPOw8M0z1SOSMnrRfQKe9w0osB_2Ks_pnwxw'
+var oauthtoken = 'ya29.c.ElpfBiSrgBUyP-5IryXPVHIky8kVHcUI4vv40br9LELZ9bgjGLpmzGvd-wRxiCFCliK5m-IiMtaCv0wdxy-wuDcwfAESWHsqCRjYZks1POEubvQtBpi-QeDu7oY'
