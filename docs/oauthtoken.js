880960552133e2e816722d25d6847a1a08622aa0
root <root@lmassa-dev.dhcp.tripadvisor.com>
2019-01-11 02:40:08 -0500

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.ElqOBuVTyEUHAHLWFbOCLvyWeGvda6o9XnP3xipliKuE6AUc15ZZIqd2PrIP1dGK4I3_rZWpWb0j0arBdY-VxHrVfKoolcqHHNPtqJPPhc_IQmsTPMbAvSRRFyQ'
+var oauthtoken = 'ya29.c.ElqOBvLEWu7kCqj9-iD4tNYFDlqg51x6P6Dc92r2Rc3l3QhI4pY_Gzmh1-5TKJ1rGIcwlbLV_d7srkxMX9yPsQoakJ9moe44TIoin-dstpnNMUMzAI-hbdBKZvo'
