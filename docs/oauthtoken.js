f09606fc6980daca6a8e81377038e3e2b5d0bc62
root <root@lmassa-dev.dhcp.tripadvisor.com>
2019-06-01 16:20:08 -0400

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.ElobB_Nt1JK2OidZMlt8xmJSZ06qTqlpjyde5w1z5jtF1MaF_4aIArOKmi5AUbd7h6YWsrertDArdbe3xnwOCvRaeyvyGWRWdjF2YE402lxxxLLPScuwKLZnVSU'
+var oauthtoken = 'ya29.c.ElobB2VuDtgNFYfhoj65RUrxhbhFoHUTypscHW-Qs9OKRTWxxtGLtp7Pqc-wRoJ9s-F76LL7kuKPN6T43sJvbXdtpA3eilT-AcCHgk1hDPFNCOt3BUe40eab_PA'
