6006471ee778746001e2af9fa0cc9155b043430e
root <root@lmassa-dev.dhcp.tripadvisor.com>
2019-09-08 08:40:05 -0400

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.Elp9B8Cj-P4fTdn3jyoyGHilhcPpwl5lwz0Iok3vHEdTbfaG34Hneoak3Ap1x2_ZY5nlMjNm_RoM9TmYC0KY-8j99SqxxZyAkO6JBEYGIyKZrZ8_x11ezlpqu_w'
+var oauthtoken = 'ya29.c.Elp9BzlbysCZC5WNdhElQL4x-rG9GwqyvpTwJSqm6BdttHcJqD51B-4XGoYIXpXRASsFNtHXRlm-xhOS8CHwgVBh0nUNhqhpj2ALKzNZ7AAjkiUzyMBgwuIBNCQ'
