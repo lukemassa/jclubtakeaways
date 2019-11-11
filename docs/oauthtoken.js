894a77718a3e88c7c27e0b2ed07185a069168aea
root <root@lmassa-dev.dhcp.tripadvisor.com>
2019-11-11 13:20:05 -0500

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.Kl6wBy5ud-RxCBDw0-AFLiYquV8KUlCdc6rGrbp5bY0Wxr-xQBrMNy_EneBPzmOmBL46GIEMJ0wi-xaa-MaSHMtyL3JGYsMWbYnWUqMRnPIfcBjaM2z2i11WSlug29ud'
+var oauthtoken = 'ya29.c.Kl6wBxZMpfnQH0JBQ8v3lxWNjhdrv5GWvGyxya_UvvyNRRbyz4xubUvHbNFk0Qnd2tGk1tMlQ59mkvuMQ6eFACre34VZ9M-k44EFCsNbWR0lCZBWAJt46OXS2NCfXCYu'
