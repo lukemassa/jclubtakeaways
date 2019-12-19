8d1123d54779a67d1c612c2c350d4330c7e7d755
root <root@lmassa-dev.dhcp.tripadvisor.com>
2019-12-19 04:20:05 -0500

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.Kl61B4bQUPIJ01wFf-jQVdDJTtdSPiO7ESk006p_w6QuqREAd9-xDRSD_JoRv-OfdgODSAkzkzmGjFcitdsu6hua_zSpZ3OG6YY0_jEKu5ZE1I3SXzhA-KJkv0FOtwOh'
+var oauthtoken = 'ya29.c.Kl61Bz3nXen5YoD4DsGvJxhwsWATcncfASAB_tSPzqCLg2RYihHkT-s_iGs-NcUPDcXqpKWmv9dnIJ_-svEh-VLFNRdcOyNBiN9VgNFpyRJ15TKWXptX2FF7Mm2eJoPw'
