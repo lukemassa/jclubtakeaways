7911e4a601e67c03f1cd49fc6c87aa66a1e01ec0
root <root@lmassa-dev.dhcp.tripadvisor.com>
2019-12-19 13:00:06 -0500

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.Kl61B0GESi8xB9LJXXAzL4-lfFCINcFy10OwWqhRh4pyq_kfVg5S1xO_0QPVUiLVHpMxmIsESr79VgszCLd9eSR9NFupNNp6P0KUjUcMreXivnTrR3V4jrWigU68KjU4'
+var oauthtoken = 'ya29.c.Kl61B3WxGtK5EjV8exatQsar9idPcYhkholBenX68XbfB_cMcPGHjuiQkKGBot7HvKG79RubwLP6Vz4rfI8ZZRMLl4geE4av62ZLeNG_Il7iGVq6UDpZvm_SqM6tsGQz'
