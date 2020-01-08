3b481bf9527f997790f98a7f221c893dd1fc16b8
root <root@lmassa-dev.dhcp.tripadvisor.com>
2020-01-08 07:20:04 -0500

    Updated oauth token

--- docs/oauthtoken.js
+++ docs/oauthtoken.js
@@ -1,1 +1,1 @@
-var oauthtoken = 'ya29.c.Kl64B8YeWAoxamTcbhlYgPh8g20tudMOSQKlKVg1YQnLzXaxx-YDYRysOrKw9XKVM3aSgRlL4cpaf_RLd7n-4OuKd8WTmNGQTrr3rny2KuY8ke-uJdyXmSVsPgLp28-b'
+var oauthtoken = 'ya29.c.Kl64B83KEt7MXI024rMwKdmRQtrB6j9hNBlk58P8u4VDV2tK1ilr3qpltw7TPGGjPHdolvru1WzVYcLTrrf-6LpUBumf37g-pYf0rfX6_6PVk0qWUSbjRK-hGc1tpx3M'
