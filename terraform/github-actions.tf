659f2cb52e58b02b12861627a521c7ab363c3e80
Luke Massa <lukefrederickmassa@gmail.com>
2026-04-25 14:43:51 -0400

    Add lambda

---
+++ terraform/github-actions.tf
@@ -0,0 +1,47 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-lambda-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:lukemassa/jclubtakeaways:ref:refs/heads/main"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_deploy" {
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "lambda:UpdateFunctionCode"
+      ]
+      Resource = aws_lambda_function.token_minter.arn
+    }]
+  })
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
